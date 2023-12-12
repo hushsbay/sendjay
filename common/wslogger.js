d14dd3dcff2362afe5bd63b9dff47b4b8de3e0dc
hushsbay <hushsbay@gmail.com>
2023-12-13 08:53:12 +0900

    .

--- common/wslogger.js
+++ common/wslogger.js
@@ -53,9 +53,8 @@ module.exports = function(logPath, title) {
          new winston.transports.Console({
             format: winston.format.combine( //json, label, timestamp, printf, simple, combine
                winston.format.colorize(), // 색깔 넣어서 출력
-               //winston.format.simple(), // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
-               winston.format.timestamp,
-               winston.format.label
+               //winston.format.simple() // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
+               winston.format.json()
             ),
          }),
       )
