c1bf47bc81f8be1f69cf434f0117b953cbf6eef0
hushsbay <hushsbay@gmail.com>
2023-12-13 08:39:42 +0900

    .

--- common/wslogger.js
+++ common/wslogger.js
@@ -15,7 +15,7 @@ module.exports = function(logPath, title) {
    const logger = winston.createLogger({
       format: combine( //로그 출력 형식 정의
          timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-         label({ label: title }),
+         label(), //label({ label: title }), 
          logFormat, //? format: combine() 에서 정의한 timestamp와 label 형식값이 logFormat에 들어가서 정의되게 된다. level이나 message는 콘솔에서 자동 정의
       ),
       transports: [ //실제 로그를 어떻게 기록을 한 것인가 정의
@@ -23,15 +23,15 @@ module.exports = function(logPath, title) {
             level: 'info', // info 레벨에선
             datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
             dirname: logDir, // 파일 경로
-            filename: `%DATE%.log`, // 파일 이름
+            filename: title + `.%DATE%.log`, // 파일 이름
             maxFiles: 3, // 최근 3일치 로그 파일을 남김
             zippedArchive: true,
          }),
          new winstonDaily({ //error 레벨 로그를 저장할 파일 설정 (info에 자동 포함되지만 일부러 따로 빼서 설정)
             level: 'error', // error 레벨에선
             datePattern: 'YYYY-MM-DD',
-            dirname: logDir + '/error', // /logs/error 하위에 저장
-            filename: `%DATE%.error.log`, // 에러 로그는 2020-05-28.error.log 형식으로 저장
+            dirname: logDir, // + '/error', // /logs/error 하위에 저장
+            filename: title + `.%DATE%.error.log`, // 에러 로그는 2020-05-28.error.log 형식으로 저장
             maxFiles: 3,
             zippedArchive: true,
          }),
@@ -41,7 +41,7 @@ module.exports = function(logPath, title) {
             level: 'error',
             datePattern: 'YYYY-MM-DD',
             dirname: logDir,
-            filename: `%DATE%.exception.log`,
+            filename: title + `.%DATE%.exception.log`,
             maxFiles: 3,
             zippedArchive: true,
          }),
