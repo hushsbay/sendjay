d90b154c0defdf850a560aa2ec19c56f653f40ae
hushsbay <hushsbay@gmail.com>
2023-12-13 07:49:34 +0900

    .

--- main/app.js
+++ main/app.js
@@ -1,9 +1,9 @@
 const config = require('./config')
 //const nodeConfig = require(config.app.nodeConfig)
 const ws = require(config.app.ws)
-const logger = require('./logger')
+const logger = require('./logger')("sdsdsd")
 
-global.logger = logger(config.app.logPath)
+global.logger = logger
 const app = ws.util.initExpressApp('public')
 const wasServer = ws.util.createWas(app, config.http.method) //not https (because of aws load balancer)
 wasServer.listen(config.http.port, () => { console.log('wasServer listening on ' + config.http.port) })
