fe341d90385d5f7dc8403ba4597685b166488968
hushsbay <hushsbay@gmail.com>
2023-12-15 16:09:20 +0900

    .

--- main/route/org/deptsearch.js
+++ main/route/org/deptsearch.js
@@ -17,13 +17,14 @@ router.post('/', async function(req, res, next) {
 		const keyword = req.body.keyword
 		const comp = (!req.body.comp || req.body.comp.toLowerCase() == 'all') ? 'all' : ws.util.toStringForInClause(req.body.comp)
 		conn = await wsmysql.getConnFromPool(global.pool)
-		sql =  "SELECT ORG_CD, ORG_NM, TOP_ORG_CD, TOP_ORG_NM "
-		sql += "  FROM JAY.Z_ORG_TBL "
-		sql += " WHERE ORG_CD IS NOT NULL " //바로 아래 조건이 where는 고려하지 말고 and만 편하게 사용하기 위한 dummy where절임
-        if (comp != 'all') sql += " AND TOP_ORG_CD IN ('" + comp + "') "
-        sql += " AND LVL > 0 " //회사 검색은 제외
-		sql += " AND ORG_NM LIKE '%" + keyword + "%' "
-        sql += " ORDER BY ORG_NM, ORG_CD "
+		sql =  "SELECT A.ORG_CD, A.ORG_NM, B.ORG_CD TOP_ORG_CD, B.ORG_NM TOP_ORG_NM "
+		sql += "  FROM JAY.Z_ORG_TBL A "
+		sql += "  LEFT OUTER JOIN JAY.Z_ORG_TBL B ON B.SEQ = CONCAT(LEFT(A.SEQ, 1), '00') "
+		sql += " WHERE A.ORG_CD IS NOT NULL " //바로 아래 조건이 where는 고려하지 말고 and만 편하게 사용하기 위한 dummy where절임
+        if (comp != 'all') sql += " AND B.ORG_CD IN ('" + comp + "') "
+        sql += " AND A.LVL > 0 " //회사 검색은 제외
+		sql += " AND A.ORG_NM LIKE '%" + keyword + "%' "
+        sql += " ORDER BY A.ORG_NM, A.ORG_CD "
 		data = await wsmysql.query(conn, sql, null)
 		len = data.length
         if (len == 0) {
