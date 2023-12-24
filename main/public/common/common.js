702cea120e2bc9c4a931998441216bb8de473343
hushsbay <hushsbay@gmail.com>
2023-12-24 21:14:58 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -57,13 +57,13 @@ function procWhenScrollStop(callback) {
                 hush.http.ajaxCall("/user/getuser", { id : user_id, imgOnly : "Y" }, function(rs) {
                     if (rs.code != hush.cons.CODE_OK) return
                     const _len = rs.list.length
+                    $("#" + tag_id).attr("downloaded", "Y")
                     if (_len == 0) return
                     const row = rs.list[0]
                     if (row.PICTURE) { //common.js참조 : 노드에서 MySql에 저장된 PICTURE(longblob)값 내리기 (2가지 방법)
                         //const url = hush.blob.setDataUrl(rs.picture, row.MIMETYPE) //방법2) base64로 변환해 내림 (사용시 서버에서도 코딩 변경 필요) + getUrlForFile() false 설정 + 서버 코딩 변경
                         const url = hush.blob.getBlobUrlForImage(row.PICTURE.data, row.MIMETYPE) //방법1) 특별히 변환하지 않고 그냥 blob으로 내림 + getUrlForFile() true 설정 + 서버 코딩 변경
                         $("#" + tag_id).attr("src", url)
-                        $("#" + tag_id).attr("downloaded", "Y")
                     }
                 })
             }
