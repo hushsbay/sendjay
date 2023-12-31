b166c741a340287befefc492af8fdd58cbd96772
hushsbay <hushsbay@gmail.com>
2024-01-01 08:43:12 +0900

    .

--- main/app.js
+++ main/app.js
@@ -11,14 +11,18 @@ const app = ws.util.initExpressApp('public')
 const wasServer = ws.util.createWas(app, config.http.method) //프로젝트 hushsbay는 aws 기반(https는 로드밸런서CLB 이용)이므로 여기서는 https가 아닌 http로 설정
 wasServer.listen(config.http.port, () => { console.log('wasServer listening on ' + config.http.port) })
 
-app.use('/user/userlist', require('./route/user/userlist')) //의도적으로 인증체크하지 않음
-app.use('/user/getuser', require('./route/user/getuser')) //의도적으로 인증체크하지 않음
-app.use('/user/setuser', require('./route/user/setuser')) //의도적으로 인증체크하지 않음
-
 app.use('/auth/login', require('./route/auth/login'))
 
-app.use('/org/orgtree', require('./route/org/orgtree')) 
-app.use('/org/empsearch', require('./route/org/empsearch')) 
-app.use('/org/deptsearch', require('./route/org/deptsearch')) 
+let rt = ['userlist', 'getuser', 'setuser']
+for (let item in rt) app.use('/user/' + item, require('./route/user/' + item)) //의도적으로 인증체크하지 않음
+//app.use('/user/userlist', require('./route/user/userlist')) //의도적으로 인증체크하지 않음
+//app.use('/user/getuser', require('./route/user/getuser')) //의도적으로 인증체크하지 않음
+//app.use('/user/setuser', require('./route/user/setuser')) //의도적으로 인증체크하지 않음
+
+rt = ['orgtree', 'empsearch', 'deptsearch']
+for (let item in rt) app.use('/org/' + item, require('./route/org/' + item))
+//app.use('/org/orgtree', require('./route/org/orgtree')) 
+//app.use('/org/empsearch', require('./route/org/empsearch')) 
+//app.use('/org/deptsearch', require('./route/org/deptsearch')) 
 
 ws.util.watchProcessError()
