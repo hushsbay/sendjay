394d4f8985fcdf7f5966f4b5f7730246c2ba09c9
hushsbay <hushsbay@gmail.com>
2023-12-31 20:28:00 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -178,7 +178,7 @@
             )}),
             ajaxFormData : (url, _data, callback, failCallback) => {
                 let data = _data
-                data.append("tokenInfo", { token : hush.http.getCookie("token"), userid : hush.http.getCookie("userid") })
+                data.append("tokenInfo", JSON.stringify({ token : hush.http.getCookie("token"), userid : hush.http.getCookie("userid") }))
                 $.ajax({url : url,
                     data : data,
                     processData : false,
