21eade5ee54f8622b62487b4dee25a838d23b10f
hushsbay <hushsbay@gmail.com>
2024-01-01 08:45:48 +0900

    .

--- main/app.js
+++ main/app.js
@@ -14,13 +14,13 @@ wasServer.listen(config.http.port, () => { console.log('wasServer listening on '
 app.use('/auth/login', require('./route/auth/login'))
 
 let rt = ['userlist', 'getuser', 'setuser']
-for (let item in rt) app.use('/user/' + item, require('./route/user/' + item)) //의도적으로 인증체크하지 않음
+for (let i; i < rt.length; i++) app.use('/user/' + rt[i], require('./route/user/' + rt[i])) //의도적으로 인증체크하지 않음
 //app.use('/user/userlist', require('./route/user/userlist')) //의도적으로 인증체크하지 않음
 //app.use('/user/getuser', require('./route/user/getuser')) //의도적으로 인증체크하지 않음
 //app.use('/user/setuser', require('./route/user/setuser')) //의도적으로 인증체크하지 않음
 
-rt = ['orgtree', 'empsearch', 'deptsearch']
-for (let item in rt) app.use('/org/' + item, require('./route/org/' + item))
+//rt = ['orgtree', 'empsearch', 'deptsearch']
+//for (let item in rt) app.use('/org/' + item, require('./route/org/' + item))
 //app.use('/org/orgtree', require('./route/org/orgtree')) 
 //app.use('/org/empsearch', require('./route/org/empsearch')) 
 //app.use('/org/deptsearch', require('./route/org/deptsearch')) 
