137bdd18ed8f1c560dccdd84dbb9809260eec1bb
hushsbay <hushsbay@gmail.com>
2024-01-26 11:31:11 +0900

    .

--- main/route/auth/login.js
+++ main/route/auth/login.js
@@ -35,20 +35,24 @@ router.post('/', async function(req, res) {
 		if (data.length == 0) {
 			ws.http.resWarn(res, '사용자아이디가 없습니다.')
 			return
-		}
-		let pwdToCompare
-		if (autologin == 'Y' && device != 'web') { //2중 체크
-			//pwd는 앱에 저장된 암호화된 상태의 값이므로 pwdToCompare도 암호화된 값 그대로 비교 필요
-			pwdToCompare = data[0].PWD
+		}		
+		if (device == 'web') {
+			//위 토큰 인증을 신뢰하고 진행함
 		} else {
-			//pwd는 암호화되지 않은 사용자 입력분 그대로이므 pwdToCompare도 디코딩 필요
-			pwdToCompare = ws.util.decrypt(data[0].PWD, nodeConfig.crypto.key)
-			console.log(pwd, pwdToCompare)
-		}
-		if (pwd != pwdToCompare) {
-			ws.http.resWarn(res, '비번이 다릅니다.')
-			return
-		}
+			let pwdToCompare
+			if (autologin == 'Y') {
+				//pwd는 앱에 저장된 암호화된 상태의 값이므로 pwdToCompare도 암호화된 값 그대로 비교 필요
+				pwdToCompare = data[0].PWD
+			} else {
+				//pwd는 암호화되지 않은 사용자 입력분 그대로이므 pwdToCompare도 디코딩 필요
+				pwdToCompare = ws.util.decrypt(data[0].PWD, nodeConfig.crypto.key)
+				console.log(pwd, pwdToCompare)
+			}
+			if (pwd != pwdToCompare) {
+				ws.http.resWarn(res, '비번이 다릅니다.')
+				return
+			}
+		}		
 		Object.assign(rs, data[0])
 		if (ws.http.deviceFrom(req) == 'web') delete rs['PWD'] //웹에서는 브라우저에서 비번저장하지 않음 (암호화된 비번도 내리지도 말기)
 		//여기는 모두 세션 쿠키로 내림. 아래 쿠키설정은 verifyUser() in common.js의 쿠키가져오기와 일치해야 함 
