a9417904d05e9f09d98dbf1e71f60213081a076a
hushsbay <hushsbay@gmail.com>
2024-02-09 18:15:15 +0900

    .

--- main/route/msngr/qry_msglist.js
+++ main/route/msngr/qry_msglist.js
@@ -14,67 +14,67 @@ router.post('/', async function(req, res) {
 	try {
 		const rs = ws.http.resInit()
 		const dateFr = ws.util.setDateAdd(new Date(), ws.cons.max_days_to_fetch)
-		const { _type, _roomid, keyword, _dt, _start, _end, _senderid, cnt } = req.body
-		const _keyword = decodeURIComponent(keyword) || ''
-		const _cnt = parseInt(cnt)
-		//const _type = req.query.type
+		let { type, roomid, keyword, dt, start, end, senderid, cnt } = req.body
+		keyword = decodeURIComponent(keyword) || ''
+		cnt = parseInt(cnt)
+		//const type = req.query.type
 		//const _userid = req.cookies.userid
-		//const _roomid = req.query.roomid
-		//const _keyword = decodeURIComponent(req.query.keyword) || ''
-		// let _dt = req.query.dt
-		// const _start = req.query.start
-		// const _end = req.query.end
-		// const _senderid = req.query.senderid
-		// const _cnt = parseInt(req.query.cnt)
+		//const roomid = req.query.roomid
+		//const keyword = decodeURIComponent(req.query.keyword) || ''
+		// let dt = req.query.dt
+		// const start = req.query.start
+		// const end = req.query.end
+		// const senderid = req.query.senderid
+		// const cnt = parseInt(req.query.cnt)
 		conn = await wsmysql.getConnFromPool(global.pool)
 		userid = await ws.jwt.chkToken(req, res, conn) //사용자 부서 위변조체크 필요없으면 세번째 인자인 conn을 빼면 됨
 		if (!userid) return
-		if (_type == 'after') {
+		if (type == 'after') {
 			sql = "SELECT CDT FROM A_MSGMST_TBL WHERE MSGID = ? AND ROOMID = ? "
-			data = await wsmysql.query(conn, sql, [_keyword, _roomid])
+			data = await wsmysql.query(conn, sql, [keyword, roomid])
 			if (data.length == 0) {
 				rs.code = ws.cons.CODE_NO_DATA
 				rs.msg = ws.cons.MSG_NO_DATA
 				ws.http.resJson(res, rs) //세번째 인자가 있으면 token 생성(갱신)해 내림
 				return
 			}
-			_dt = data[0].CDT
+			dt = data[0].CDT
 		}
 		let arg 
-		console.log(dateFr, _type, userid, _roomid, _keyword, _dt, _start, _end, _senderid, _cnt)
+		console.log(dateFr, type, userid, roomid, keyword, dt, start, end, senderid, cnt)
 		sql = "SELECT A.MSGID, A.CDT, A.SENDERID, A.SENDERNM, B.RECEIVERID, A.BODY, A.BUFFER, A.REPLY, A.TYP TYPE, B.STATE, A.FILESTATE, "
 		sql += "		  CASE WHEN A.BUFFER IS NULL THEN NULL ELSE 'Y' END BUFFERSTR, " 
 		sql += "          (SELECT COUNT(*) FROM A_MSGDTL_TBL WHERE MSGID = B.MSGID AND ROOMID = B.ROOMID AND STATE = '') CNT "
 		sql += "     FROM A_MSGDTL_TBL B "
 		sql += "	 LEFT OUTER JOIN A_MSGMST_TBL A ON B.MSGID = A.MSGID "
 		sql += "	WHERE B.ROOMID = ? AND B.RECEIVERID = ? AND B.STATE IN ('', 'R') AND A.CDT >= ? "
-		if (_type == 'search') {
-			sql += "  AND A.BODY LIKE '%" + _keyword + "%' "
+		if (type == 'search') {
+			sql += "  AND A.BODY LIKE '%" + keyword + "%' "
 			sql += "ORDER BY A.CDT LIMIT 0, ? "
-			arg = [_roomid, userid, dateFr, _cnt]
-			console.log(sql, _roomid, userid, dateFr, _cnt)
-		} else if (_type == 'etc') {
+			arg = [roomid, userid, dateFr, cnt]
+			console.log(sql, roomid, userid, dateFr, cnt)
+		} else if (type == 'etc') {
 			sql += "  AND (A.TYP IN ('file', 'flink', 'image') OR (A.TYP = 'talk' AND (A.BODY LIKE '%http://%' OR A.BODY LIKE '%https://%'))) "
 			sql += "  AND A.BODY <> '" + ws.cons.cell_revoked + "' "
 			sql += "ORDER BY A.CDT LIMIT 0, ? "
-			arg = [_roomid, userid, dateFr, _cnt]
-		} else if (_type == 'result') {
+			arg = [roomid, userid, dateFr, cnt]
+		} else if (type == 'result') {
 			sql += "  AND A.CDT >= ? AND A.CDT < ? "
 			sql += "ORDER BY A.CDT DESC "
-			arg = [_roomid, userid, dateFr, _start, _end]
-		} else if (_type == 'onlyone') {
+			arg = [roomid, userid, dateFr, start, end]
+		} else if (type == 'onlyone') {
 			sql += "  AND A.CDT < ? "
 			sql += "  AND B.SENDERID = ? "
 			sql += "ORDER BY A.CDT DESC LIMIT 0, ? "
-			arg = [_roomid, userid, dateFr, _dt, _senderid, _cnt]
-		} else if (_type == 'after') { //from mobile (before noti). UDT needed for checking revoked msg when connect after disconnect
+			arg = [roomid, userid, dateFr, dt, senderid, cnt]
+		} else if (type == 'after') { //from mobile (before noti). UDT needed for checking revoked msg when connect after disconnect
 			sql += "  AND (A.CDT > ? OR A.UDT > ?) "
 			sql += "ORDER BY A.CDT "
-			arg = [_roomid, userid, dateFr, _dt, _dt]			
+			arg = [roomid, userid, dateFr, dt, dt]			
 		} else { //normal
 			sql += "  AND A.CDT < ? "
 			sql += "ORDER BY A.CDT DESC LIMIT 0, ? "
-			arg = [_roomid, userid, dateFr, _dt, _cnt]
+			arg = [roomid, userid, dateFr, dt, cnt]
 		}
 		console.log("==============")
 		data = await wsmysql.query(conn, sql, arg)
