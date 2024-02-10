cac8536afb42f6330202c6959e5023892486d665
hushsbay <hushsbay@gmail.com>
2024-02-10 18:22:45 +0900

    .

--- main/public/app/msngr/main_common.js
+++ main/public/app/msngr/main_common.js
@@ -938,7 +938,7 @@ var funcSockEv = { //needs to be public
                     hush.noti.procNoti(data.roomid, data)
                 }
                 if (!runFromStandalone) return
-                setTimeout(() => getPortalList({ type: "row", roomid : data.roomid }), 10)
+                setTimeout(() => getPortalList({ type: "row", roomid : data.roomid }), 3000)
             }
         } catch (ex) {
             await hush.msg.alert("[main]sock_ev_send_msg: " + ex.message)
