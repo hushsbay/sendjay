d6a952f615b5db9fcbebc46cd16109e29e83ab32
hushsbay <hushsbay@gmail.com>
2024-02-17 12:29:52 +0900

    .

--- main/route/msngr/get_opengraph.js
+++ main/route/msngr/get_opengraph.js
@@ -1,7 +1,7 @@
 const config = require('../../config')
 const ws = require(config.app.ws)
 const express = require('express')
-const ogs = require('open-graph-scraper') //https://balmostory.tistory.com/50
+const ogs = require('open-graph-scraper') //@4.7.1 적용. 최신 버전은 오류 발생 (fix 필요) https://balmostory.tistory.com/50
 const router = express.Router()
 
 const proc = (req) => {
@@ -10,15 +10,12 @@ const proc = (req) => {
 			const rs = ws.http.resInit()
 			let _ret = { msgid : req.body.msgid, url : req.body.url, ogImg : '', ogTitle : '', ogDesc : '' }
 			const option = { url : req.body.url, timeout : 5000, encoding : 'utf-8', followAllRedirects : true, maxRedirects : 5, blacklist : [ ] }
-			console.log("!!!!!!!!!!!!!!!4", req.body.url, req.body.msgid)
 			ogs(option, function (error, result) {
 				if (error) { //true. The error it self is inside the results object.
-					console.log("!!!!!!!!!!!!!!!2")
 					rs.result = _ret			
 					resolve(rs)
 				} else {  
 					try {
-						console.log("!!!!!!!!!!!!!!!3")
 						_ret.ogImg = result.ogImage.url
 						_ret.ogTitle = result.ogTitle
 						_ret.ogDesc = result.ogDescription
@@ -39,7 +36,6 @@ const proc = (req) => {
 router.post('/', async (req, res) => {
 	req.title = 'get_opengraph.post'
 	try {
-		console.log("!!!!!!!!!!!!!!!1")
 		const rs = await proc(req)
 		res.json(rs)
 	} catch (ex) {
