e3e1eae6db55cdf3a0b9642a6a054a6148f5b8df
hushsbay <hushsbay@gmail.com>
2024-02-18 19:00:14 +0900

    .

--- main/socket/set_env.js
+++ main/socket/set_env.js
@@ -16,17 +16,17 @@ module.exports = async function(socket, param) {
 			const _roomid = param.data.roomid
 			if (_kind == 'noti') {
 				await wsmysql.query(conn, "UPDATE A_ROOMDTL_TBL SET NOTI = ? WHERE ROOMID = ? AND USERID = ? ", [_value, _roomid, _userid]) 
-				socket.emit(com.cons.sock_ev_common, param)	
+				socket.emit(ws.cons.sock_ev_common, param)	
 				ws.sock.sendToMyOtherSocket(socket, param)
 			} else { //mobile only
 				await wsmysql.query(conn, "UPDATE A_ROOMDTL_TBL SET DISPMEM = ? WHERE ROOMID = ? AND USERID = ? ", [_value, _roomid, _userid])
-				socket.emit(com.cons.sock_ev_common, param)	 
+				socket.emit(ws.cons.sock_ev_common, param)	 
 			}					
 		} else if (_kind == 'userinfo') { //No need to update table since it was already updated. You just broadcast it inside namespace.
-			ws.sock.broadcast(socket, com.cons.sock_ev_set_env, param.data, 'all')
+			ws.sock.broadcast(socket, ws.cons.sock_ev_set_env, param.data, 'all')
 		} //ws.sock.warn(null, socket, _logTitle, com.cons.rs + JSON.stringify(param))
 	} catch (ex) {
-		ws.sock.warn(com.cons.sock_ev_alert, socket, _logTitle, ex)
+		ws.sock.warn(ws.cons.sock_ev_alert, socket, _logTitle, ex)
 	} finally {
 		wsmysql.closeConn(conn, _logTitle)
 	}
