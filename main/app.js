76706bd7063e19fd7efebec8abaf6f1d74b5527a
hushsbay <hushsbay@gmail.com>
2024-02-24 06:55:05 +0900

    .

--- main/app.js
+++ main/app.js
@@ -121,7 +121,8 @@ rt = [
 	'chk_redis', 'qry_unread', 'qry_userlist', 'qry_orgtree', 'qry_portal', 'qry_msglist', 'get_roominfo', 'proc_file', 'proc_image',
 	'get_msginfo', 'get_opengraph', 'proc_env', 'proc_picture'
 ] 
-for (let i = 0; i < rt.length; i++) app.use('/msngr/' + rt[i], require('./route/msngr/' + rt[i])) 
+//for (let i = 0; i < rt.length; i++) app.use('/msngr/' + rt[i], require('./route/msngr/' + rt[i])) 
+app.use('/msngr/:routing', require('./route/msngr/:routing')) 
 
 proc()
 async function proc() {
