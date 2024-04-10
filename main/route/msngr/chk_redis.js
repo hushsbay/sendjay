b5404ae1ddf49c4ae187ec3dfbf6a9ddf644d499
hushsbay <hushsbay@gmail.com>
2024-04-10 21:17:30 +0900

    .

--- main/route/msngr/chk_redis.js
+++ main/route/msngr/chk_redis.js
@@ -15,13 +15,13 @@ router.post('/', async function(req, res) {
 		const { type, userkey, winid } = req.body
 		userid = await ws.jwt.chkToken(req, res) //사용자 부서 위변조체크 필요없으면 세번째 인자인 conn을 빼면 됨
 		if (!userid) return	
-		const pattern = ws.cons.key_str_winid + userkey + ws.cons.easydeli //eg) $$ + W + W__USERID;
-		const uwKey = pattern + winid //eg) $$WW__USERID;xxxxxx20241231010159
+		const pattern = ws.cons.key_str_winid + userkey + ws.cons.easydeli //$$ + W + W__USERID;
+		const uwKey = pattern + winid //$$WW__USERID;winid
+		const _dt = ws.util.getCurDateTimeStr(true)
 		const stream = global.store.scanStream({ match : pattern + '*', count : ws.cons.scan_stream_cnt }) 
 		//console.log(type, "##############", userkey, winid, pattern, uwKey)
 		stream.on('data', async (resultKeys) => { //resultKeys is an array of strings representing key names
 			if (type == "chk_embeded") { //웹메신저 자동실행을 위한 마지막 체크임
-				const _dt = ws.util.getCurDateTimeStr(true)
 				if (resultKeys.length == 0) { //console.log(type, userkey, winid, pattern, uwKey, "====new")
 					await global.store.set(uwKey, _dt)
 					rs.result = "new" //새로운 winner
@@ -43,7 +43,7 @@ router.post('/', async function(req, res) {
 				}
 			} else if (type == "set_new") { //manual 실행시 무조건 키 setting함 (auto로 들어오는 embeded일 경우는 여기로 오면 안됨 - 무한루프)
 				for (let item of resultKeys) await global.store.del(item)
-				await global.store.set(uwKey, winid) //console.log(type, userkey, winid, pattern, uwKey)
+				await global.store.set(uwKey, _dt) //await global.store.set(uwKey, winid) //console.log(type, userkey, winid, pattern, uwKey)
 				rs.result = "new" //새로운 winner		
 			}
 			rs.userip = req.clientIp //소켓서버에서 파악이 힘들어 미리 클라이언트로 내려 요청시 포함하도록 하려 함
