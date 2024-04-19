46f766d5e77e70452bd880ddbd10d6a5ad547da0
hushsbay <hushsbay@gmail.com>
2024-04-20 04:20:20 +0900

    .

--- main/public/app/msngr/chat_common.js
+++ main/public/app/msngr/chat_common.js
@@ -1441,7 +1441,8 @@ const dialogRoomRename = (_type) => {
     hush.msg.dialogInputBox("새로운 방명을 입력하시기 바랍니다.<br>기존 방명을 지우려면 빈칸을 입력하면 됩니다.", g_title, { 
         "확인": function() {                             
             const _newName = hush.msg.dialogGetInput().trim()
-            if (!hush.util.chkFieldVal(_newName, "병명", 0, 100)) return
+            if (!hush.util.chkFieldVal(_newName, "병명", 1, 100)) return
+            debugger
             const rq = { type : _type, roomname : _newName, roomid : g_roomid } //roomid needed since
             if (hush.webview.ios) { 
             } else if (hush.webview.and) {
