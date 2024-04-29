a25b88e5e28546ae25b53705e65666645b211b2d
hushsbay <hushsbay@gmail.com>
2024-04-29 10:35:52 +0900

    .

--- main/public/app/msngr/main_common.js
+++ main/public/app/msngr/main_common.js
@@ -1111,8 +1111,8 @@ function procNewChat(useridArr) { //invoked from index.html and jay_main.js : Mo
 ////////////////////////////////////////////////////////////////////////mobile webview
 const startFromWebView = async (from, obj, rs) => {
     try {
-        hush.auth.setCookieForUser(obj, true)
         debugger
+        hush.auth.setCookieForUser(obj, true)        
         hush.user = hush.auth.setUser(obj.token)        
         SetUserVar()
         if (g_win_type) {
