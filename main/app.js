1874b9d6d59d11f3b69b6cd16829ba05d509fb51
hushsbay <hushsbay@gmail.com>
2024-04-30 15:34:38 +0900

    .

--- main/app.js
+++ main/app.js
@@ -42,7 +42,7 @@ const socketServer = ws.util.createWas(appSocket, config.http.method) //not http
 const io = new Server(socketServer, { allowEIO3: false, autoConnect: true, pingTimeout: PING_TIMEOUT, pingInterval: PING_INTERVAL, cors: { origin: config.app.corsSocket, methods: ["GET", "POST"] }})
 io.adapter(redisAdapter(global.pub, sub))
 io.listen(config.sock.port)
-global.jay = io.of('/' + config.sock.namespace).adapter //global.jay = io.of('/' + config.sock.namespace)
+global.jay = io.of('/' + config.sock.namespace)
 global.jay.on('connection', async (socket) => {
 	const _logTitle = 'connect'	
 	try {
@@ -125,7 +125,7 @@ for (let i = 0; i < rt.length; i++) app.use('/msngr/' + rt[i], require('./route/
 
 proc()
 async function proc() {
-    const sockets = await global.jay.sockets(new Set()) //const sockets = await global.jay.adapter.sockets(new Set()) //https://socket.io/docs/v4/adapter/
+    const sockets = await global.jay.adapter.sockets(new Set()) //https://socket.io/docs/v4/adapter/
 	//const sockets1 = await global.jay.sockets //위 아래 둘 다 각 서버의 소켓 카운트만 가능
     //console.log('socket count :', sockets.size) //, sockets1.size)
 	const stream = global.store.scanStream({ match : ws.cons.key_str_socket + '*', count: ws.cons.scan_stream_cnt })
