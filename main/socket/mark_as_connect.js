71b57abbf39be5c21ba4b9e783d67bd2ab7d0801
hushsbay <hushsbay@gmail.com>
2024-05-08 11:18:21 +0900

    .

--- main/socket/mark_as_connect.js
+++ main/socket/mark_as_connect.js
@@ -4,6 +4,7 @@ const ws = require(config.app.ws)
 module.exports = async function(socket, param) {
 	const _logTitle = param.ev
 	try {
+		console.log("mark_as_connect")
 		socket.emit(ws.cons.sock_ev_common, param)
 	} catch (ex) {
 		ws.sock.warn(ws.cons.sock_ev_alert, socket, _logTitle, ex)
