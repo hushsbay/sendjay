60aa2bcf56cb41c6a74a4af7f1c934cc9e56324b
hushsbay <hushsbay@gmail.com>
2024-05-24 15:41:51 +0900

    .

--- main/route/msngr/get_msginfo.js
+++ main/route/msngr/get_msginfo.js
@@ -107,8 +107,6 @@ router.get('/', async function(req, res) {
 			return
 		}
 		if (data[0].TYPE == 'image') {
-			//rs.buffer = data[0].BUFFER //rs.bufferStr = (data[0].BUFFER) ? Buffer.from(data[0].BUFFER, 'binary').toString('base64') : null
-			//resolve(rs)
 			const ret = await ws.util.chkAccessUserWithTarget(conn, userid, msgid, '')
 			if (ret != '') throw new Error(ret)
 			rs.list = data
@@ -130,44 +128,6 @@ router.get('/', async function(req, res) {
 			} else {
 				ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
 			}
-		} else if (data[0].TYPE == 'file' || data[0].TYPE == 'flink') { //almost same as get_sublink.js 
-			let fileToProc
-			const _fileStr = body.split(ws.cons.deli)
-			const objFileStr = ws.util.getFileNameAndExtension(_fileStr[0])
-			if (ws.cons.sublink_ext_video.includes(objFileStr.ext)) {
-				fileToProc = _fileStr[0] + ws.cons.sublink_result_img
-			} else if (ws.cons.sublink_ext_image.includes(objFileStr.ext)) {
-				fileToProc = _fileStr[0]
-			} else {
-				rs.list = data
-				ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
-				return
-			}
-			const ret = await ws.util.chkAccessUserWithTarget(conn, userid, msgid, 'file', _fileStr[0])
-			if (ret != '') throw new Error(ret)
-			const _filepath = config.app.uploadPath + '/' + fileToProc
-			fs.stat(_filepath, function(err, stat) {
-				if (err) {
-					rs.list = data
-					ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
-				} else {
-					if (stat && stat.isFile() && stat.size <= ws.cons.max_size_to_sublink) { 
-						fs.readFile(_filepath, function(err1, result) {
-							if (!err1) data[0].BUFFER = result									
-							rs.list = data
-							ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
-						})
-					} else {
-						rs.list = data
-						ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
-					}
-				}					
-			})
-		} else {
-			const ret = await ws.util.chkAccessUserWithTarget(conn, userid, msgid, '')
-			if (ret != '') throw new Error(ret)
-			rs.list = data
-			ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
 		}
 	} catch (ex) {
 		ws.http.resException(req, res, ex)
