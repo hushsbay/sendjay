4c2eb958a6826abf11e6e897e91162479cef61b9
hushsbay <hushsbay@gmail.com>
2024-05-28 06:49:38 +0900

    .

--- main/thread/worker.js
+++ main/thread/worker.js
@@ -19,9 +19,10 @@ async function proc() {
         conn = await wsmysql.getConnFromPool(global.pool)
         await wsmysql.txBegin(conn) //1) delete files to be expired 
         //sql = "SELECT MSGID, ROOMID, BODY FROM A_MSGMST_TBL WHERE TYP in ('file', 'flink') AND FILESTATE < sysdate() AND FILESTATE <> ? AND BODY <> ? "
-        sql = "SELECT MSGID, ROOMID, BODY FROM A_MSGMST_TBL WHERE TYP in ('file', 'flink') AND FILESTATE < '2024-01-01' AND FILESTATE <> ? AND BODY <> ? "
-        data = await wsmysql.query(conn, sql, [ws.cons.file_expired, ws.cons.cell_revoked])
+        sql = "SELECT MSGID, ROOMID, BODY FROM A_MSGMST_TBL WHERE TYP in ('file', 'flink') AND FILESTATE like '2024-05-28%' AND FILESTATE <> ? "
+        data = await wsmysql.query(conn, sql, [ws.cons.file_expired])
         _len = data.length
+        console.log(_len+"****")
         for (let i = 0; i < _len; i++) {
             const _msgid = data[i].MSGID
             const _roomid = data[i].ROOMID
@@ -31,28 +32,28 @@ async function proc() {
             await wsmysql.query(conn, "UPDATE A_MSGMST_TBL SET FILESTATE = ? WHERE MSGID = ? AND ROOMID = ? ", [ws.cons.file_expired, _msgid, _roomid]) 
             deleteFileAndRemoveEmptyFolderFromChild(_path, _filename, 'expiry')
         } //2) remove garbage files (when browser closing, when aborted ..)
-        sql = "SELECT MSGID, ROOMID, SENDERID, BODY FROM A_FILELOG_TBL WHERE UDT = '' AND CDT < DATE_ADD(sysdate(), INTERVAL -24 HOUR) "
-        data = await wsmysql.query(conn, sql, null)
-        _len = data.length
-        for (let i = 0; i < _len; i++) {
-            const _msgid = data1[i].MSGID
-            const _roomid = data1[i].ROOMID
-            const _senderid= data1[i].SENDERID
-            const _filename = data1[i].BODY
-            const _path = config.app.uploadPath + '/' + _roomid + '/' + _senderid + "/" + _filename
-            const sql2 = "SELECT COUNT(*) CNT FROM A_MSGMST_TBL WHERE MSGID = ? AND ROOMID = ? AND BODY LIKE '%" + _filename + "%' "
-            const data2 = await wsmysql.query(conn, sql2, [_msgid, _roomid])
-            await wsmysql.query(conn, "UPDATE A_MSGMST_TBL SET UDT = sysdate() WHERE MSGID = ? AND ROOMID = ? ", [_msgid, _roomid]) 
-            if (data2[0].CNT == 1) continue //no garbage 
-            deleteFileAndRemoveEmptyFolderFromChild(_path, _filename, 'garbage')
-        }
+        // sql = "SELECT MSGID, ROOMID, SENDERID, BODY FROM A_FILELOG_TBL WHERE UDT = '' AND CDT < DATE_ADD(sysdate(), INTERVAL -24 HOUR) "
+        // data = await wsmysql.query(conn, sql, null)
+        // _len = data.length
+        // for (let i = 0; i < _len; i++) {
+        //     const _msgid = data1[i].MSGID
+        //     const _roomid = data1[i].ROOMID
+        //     const _senderid= data1[i].SENDERID
+        //     const _filename = data1[i].BODY
+        //     const _path = config.app.uploadPath + '/' + _roomid + '/' + _senderid + "/" + _filename
+        //     const sql2 = "SELECT COUNT(*) CNT FROM A_MSGMST_TBL WHERE MSGID = ? AND ROOMID = ? AND BODY LIKE '%" + _filename + "%' "
+        //     const data2 = await wsmysql.query(conn, sql2, [_msgid, _roomid])
+        //     await wsmysql.query(conn, "UPDATE A_MSGMST_TBL SET UDT = sysdate() WHERE MSGID = ? AND ROOMID = ? ", [_msgid, _roomid]) 
+        //     if (data2[0].CNT == 1) continue //no garbage 
+        //     deleteFileAndRemoveEmptyFolderFromChild(_path, _filename, 'garbage')
+        // }
         await wsmysql.txCommit(conn)
     } catch (ex) {
         if (conn) await wsmysql.txRollback(conn)
         global.logger.error(TITLE + ': error\n' + ex.stack)
     } finally {
         wsmysql.closeConn(conn, TITLE)
-        setTimeout(() => { proc() }, 5000) //Test
+        setTimeout(() => { proc() }, 3000) //Test
     }
 }
 
@@ -60,10 +61,10 @@ function deleteFileAndRemoveEmptyFolderFromChild(_path, _filename, type) {
     try {
         const stat = fs.statSync(_path)
         if (stat && stat.isFile()) { 
-            console.log(TITLE + ': ' + type + ': ' + _path)
+            console.log(TITLE + ': ' + type + ': ' + _path + ": " + _filename)
             fs.unlinkSync(_path)
             const pathObj = ws.util.getFileNameAndExtension(_path)
-            if (ws.cons.sublink_ext_video.includes(pathObj.ext)) { //related with ffmpeg (streaming)
+            if (ws.cons.sublink_ext_video.includes(pathObj.ext)) { //ffmpeg으로 생성된 확장자 파일도 삭제
                 try {
                     const imgPath = _path + ws.cons.sublink_result_img
                     const stat = fs.statSync(imgPath)
@@ -81,6 +82,7 @@ function deleteFileAndRemoveEmptyFolderFromChild(_path, _filename, type) {
                 } else {
                     brr.push(brr.join('/') + '/' + arr[i])
                 }
+                console.log("brr===>"+brr[brr.length-1])
             }
             for (let j = brr.length - 1; j >= 0; j--) { //remove empty folder from child
                 const files = fs.readdirSync(config.app.uploadPath + '/' + brr[j])
