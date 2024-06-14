4f0c3a64b8b809a35735c58fb79e6f637d0b0b7a
hushsbay <hushsbay@gmail.com>
2024-06-14 16:29:43 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -91,7 +91,7 @@
             handling : ".. ", //handling..
             no_response : "전송확인",
             retry_sending : "retry sending",
-            sending_failure : "오류보기",
+            sending_failure : "문제발생",
             left : " left this room",
             param_webview_and : "webview=and",
             param_webview_ios : "webview=ios",
