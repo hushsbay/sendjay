1be62dca533a2f3a5ff60555f6d8bc0bc1fc7109
hushsbay <hushsbay@gmail.com>
2024-07-24 10:28:15 +0900

    .

--- main/route/org/interfaceToDept.js
+++ main/route/org/interfaceToDept.js
@@ -15,7 +15,7 @@ router.post('/', async function(req, res) {
 	try {
 		const rs = ws.http.resInit()	
 		conn = await wsmysql.getConnFromPool(global.pool)
-		console.log(req.body.userid, req.body.token)
+		const dept = req.body.dept
 		const objToken = await ws.jwt.chkToken(req, res) //res : 오류시 바로 클라이언트로 응답. conn : 사용자 조직정보 위변조체크
 		const userid = objToken.userid
 		if (!userid) {
@@ -23,28 +23,27 @@ router.post('/', async function(req, res) {
 			return
 		}
 		if (!ws.http.ipChk(config.app.ipAccess, req.clientIp)) throw new Error('client ip not allowed')
-		// sql = "SELECT COUNT(*) CNT FROM Z_USER_TBL WHERE USER_ID = ? "
-		// data = await wsmysql.query(conn, sql, [userid])
-		// if (data.length == 0) {
-		// 	ws.http.resWarn(res, ws.cons.MSG_NO_DATA, true, ws.cons.CODE_NO_DATA)
-		// 	return
-		// }
-		// let _nicknm = decodeURIComponent(req.body.nicknm) || ''
-		// const _job = decodeURIComponent(req.body.job) || ''
-		// const _abcd = decodeURIComponent(req.body.abcd) || ''
-		// const _abnm = decodeURIComponent(req.body.abnm) || ''
-		// const _notioff = req.body.notioff
-		// const _fr = req.body.fr
-		// const _to = req.body.to
-		// const _bodyoff = req.body.bodyoff
-		// const _senderoff = req.body.senderoff
-		// sql = "UPDATE Z_USER_TBL "
-		// sql += "  SET NICK_NM = ?, JOB = ?, AB_CD = ?, AB_NM = ?, NOTI_OFF = ?, " 
-		// sql += "      BODY_OFF = ?, SENDER_OFF = ?, TM_FR = ?, TM_TO = ?, MODR = ?, MODDT = sysdate(6) "
-		// sql += "WHERE USER_ID = ? "
-		// await wsmysql.query(conn, sql, [_nicknm, _job, _abcd, _abnm, _notioff, _bodyoff, _senderoff, _fr, _to, userid, userid])
-		ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
+		if (!Array.isArray(dept)) throw new Error('dept 파라미터는 배열이어야 합니다.') 
+		if (dept.length == 0) throw new Error('배열의 길이가 0입니다.')
+		await wsmysql.txBegin(conn)
+		let dtkey = dept[0].DTKEY ? dept[0].DTKEY : ws.util.getCurDateTimeStr()
+		sql = "SELECT COUNT(*) CNT FROM Z_INTORG_TBL WHERE DTKEY = ? "
+		data = await wsmysql.query(conn, sql, [dtkey])
+		len = data.length
+		if (len > 0) {
+			sql = "DELETE FROM Z_INTORG_TBL WHERE DTKEY = ? "
+			await wsmysql.query(conn, sql, [dtkey])
+		}
+		for (let i = 0; i < len; i++) {			
+			sql = "INSERT INTO Z_INTORG_TBL (DTKEY, ORG_CD, ORG_NM, SEQ, LVL, INUSE, IS_SYNC) "
+			sql += " VALUES (?, ?, ?, ?, ?, ?, ?) "
+			await wsmysql.query(conn, sql, [dtkey, data[i].ORG_CD, data[i].ORG_NM, data[i].SEQ, data[i].LVL, data[i].INUSE, 'Y'])
+		}
+		rs.dtkey = dtkey
+		await wsmysql.txCommit(conn)
+		ws.http.resJson(res, rs, userid) //세번째 인자(userid) 있으면 token 갱신
 	} catch (ex) {
+		if (conn) await wsmysql.txRollback(conn)
 		ws.http.resException(req, res, ex)
 	} finally {
 		wsmysql.closeConn(conn, req.title)
