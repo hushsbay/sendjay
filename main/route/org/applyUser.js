b88c9b3e48c8eadaa42c8541b5681ffd20adc0a3
hushsbay <hushsbay@gmail.com>
2024-07-26 07:52:12 +0900

    .

--- main/route/org/applyUser.js
+++ main/route/org/applyUser.js
@@ -16,7 +16,6 @@ router.post('/', async function(req, res) {
 		const rs = ws.http.resInit()	
 		conn = await wsmysql.getConnFromPool(global.pool)
 		const dtkey = req.body.key
-		console.log(dtkey, "----")
 		const objToken = await ws.jwt.chkToken(req, res) //res : 오류시 바로 클라이언트로 응답. conn : 사용자 조직정보 위변조체크
 		const userid = objToken.userid
 		if (!userid) {
@@ -39,29 +38,27 @@ router.post('/', async function(req, res) {
 				sql = "UPDATE Z_USER_TBL SET "
 				sql += " USER_NM = ?, ORG_CD = ?, ORG_NM = ?, TOP_ORG_CD = ?, TOP_ORG_NM = ?, JOB  = ?, TEL_NO = ?, AB_CD  = ?, AB_NM = ? "
 				sql += "WHERE USER_ID = ? "
-				data = await wsmysql.query(conn, sql, [
+				await wsmysql.query(conn, sql, [
 					data1[0].USER_NM, data1[0].ORG_CD, data1[0].ORG_NM, data1[0].TOP_ORG_CD, data1[0].TOP_ORG_NM, data1[0].JOB, data1[0].TEL_NO, data1[0].AB_CD, data1[0].AB_NM, _userid
 				])	
 			} else {
 				sql = "DELETE FROM Z_USER_TBL WHERE USER_ID = ? AND IS_SYNC = 'Y' "
-				data = await wsmysql.query(conn, sql, [_userid])
+				await wsmysql.query(conn, sql, [_userid])
 			}
 		}
 		//2. Z_INTORG에 있는데 Z_ORG_TBL에 없으면 신규(추가)분이므로 넣기
 		sql = "SELECT USER_ID, USER_NM, ORG_CD, ORG_NM, TOP_ORG_CD, TOP_ORG_NM, JOB, TEL_NO, AB_CD, AB_NM FROM Z_INTUSER_TBL WHERE DTKEY = ? "
-		const data2 = await wsmysql.query(conn, sql, [dtkey])
-		len = data2.length
+		const data = await wsmysql.query(conn, sql, [dtkey])
+		len = data.length
 		for (let i = 0; i < len; i++) {
-			console.log(JSON.stringify(data2[i]))
-			const _userid = data2[i].USER_ID
-			console.log(_userid, "=====")
+			const _userid = data[i].USER_ID
 			sql = "SELECT * FROM Z_USER_TBL WHERE USER_ID = ? "
 			const data1 = await wsmysql.query(conn, sql, [_userid])
 			if (data1.length == 0) {
 				sql = "INSERT INTO Z_USER_TBL (PWD, USER_ID, ID_KIND, USER_NM, ORG_CD, ORG_NM, TOP_ORG_CD, TOP_ORG_NM, JOB, TEL_NO, AB_CD, AB_NM) "
 				sql += " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "
-				data = await wsmysql.query(conn, sql, [ //PWD는 나중에 제거하기 (drop table후)
-					'', data2[0].USER_ID, 'U', data2[0].USER_NM, data2[0].ORG_CD, data2[0].ORG_NM, data2[0].TOP_ORG_CD, data2[0].TOP_ORG_NM, data2[0].JOB, data2[0].TEL_NO, data2[0].AB_CD, data2[0].AB_NM
+				await wsmysql.query(conn, sql, [ //PWD는 나중에 제거하기 (drop table후)
+					'', _userid, 'U', data[i].USER_NM, data[i].ORG_CD, data[i].ORG_NM, data[i].TOP_ORG_CD, data[i].TOP_ORG_NM, data[i].JOB, data[i].TEL_NO, data[i].AB_CD, data[i].AB_NM
 				])	
 			} else {
 				//위 1.에서 처리됨. 동기화되는 아이디가 아닌데 수동으로 이미 만든 아이디가 있을 수 있음 (오류처리하지 않고 일단 넘어감)
