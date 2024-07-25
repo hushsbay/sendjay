8898fd4d82d9dd6e4a5623a385e70d67abbe4ae1
hushsbay <hushsbay@gmail.com>
2024-07-25 14:21:54 +0900

    .

--- main/route/org/interfaceKeys.js
+++ main/route/org/interfaceKeys.js
@@ -13,16 +13,16 @@ router.use(function(req, res, next) {
 router.post('/', async function(req, res) {
 	let conn, sql, data, len
 	try {
-		const rs = ws.http.resInit()	
+		const rs = ws.http.resInit()
+		rs.dept = []
+		rs.user = []
 		conn = await wsmysql.getConnFromPool(global.pool)
 		sql = "SELECT DISTINCT DTKEY FROM Z_INTORG_TBL ORDER BY DTKEY DESC LIMIT 0, 10 "
 		data = await wsmysql.query(conn, sql, null)
-		len = data.length
-		if (len > 0) rs['dept'] = rs.data
+		if (data.length > 0) rs.dept = data
 		sql = "SELECT DISTINCT DTKEY FROM Z_INTUSER_TBL ORDER BY DTKEY DESC LIMIT 0, 10 "
 		data = await wsmysql.query(conn, sql, null)
-		len = data.length
-		if (len > 0) rs['user'] = rs.data
+		if (data.length > 0) rs.user = data
 		ws.http.resJson(res, rs) //세번째 인자(userid) 있으면 token 갱신
 	} catch (ex) {
 		ws.http.resException(req, res, ex)
