5a2452bfaae42c3128fe70449716687ed82da1e8
hushsbay <hushsbay@gmail.com>
2024-08-05 18:54:52 +0900

    .

--- main/public/app/msngr/main_common.js
+++ main/public/app/msngr/main_common.js
@@ -78,6 +78,7 @@ const procMenuTop = async (_mode, _mode_people) => {
             $("#fr_chat").hide()
             $("#fr_setting").css("display", "flex")
             $(".setting").show()
+            debugger
             const rs = await hush.auth.verifyUser()
             if (rs.code == hush.cons.CODE_OK) procSetting("load", rs, true)
         }
