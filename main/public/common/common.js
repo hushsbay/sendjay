02462d7f363e00b6060e1b109015da6b3a3bbb63
hushsbay <hushsbay@gmail.com>
2024-08-08 09:20:57 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -163,6 +163,7 @@
                 return rs
             },  
             refreshToken : async () => { //모바일(웹뷰)에서는 디바이스가 대기모드 등으로 들어갈 때 http call이 멈추게 됨을 유의
+                return
                 if (hush.http.chkOnline("none")) {
                     try {
                         const rs = await hush.http.ajax("/auth/refresh_token", {}, true)
