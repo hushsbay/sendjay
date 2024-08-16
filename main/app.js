f9f0663a7fbfa8de5deeb59f25e6c7413cf4d743
hushsbay <hushsbay@gmail.com>
2024-08-16 15:44:32 +0900

    .

--- main/app.js
+++ main/app.js
@@ -41,7 +41,8 @@ if (config.app.mainserver == 'Y') global.store.flushdb(function(err, result) { c
 const appSocket = ws.util.initExpressApp()
 const socketServer = ws.util.createWas(appSocket, config.http.method) //not https (because of aws elastic load balancer)
 const io = new Server(socketServer, { //여기 Server는 socket.io
-	//autoConnect to false (모바일 connect후 로그인/로깅시 http호출 여러번 발생해서 false로 변경) : 웹은 끊어지면 다른 탭에서 연결, 모바일도 SocketIO.invoke 새로하므로 false로 해도 무방
+	//autoConnect to false (모바일 connect후 로그인/로깅시 http호출 여러번 발생해서 false로 변경)
+	//웹은 끊어지면 다른 탭에서 연결, 모바일도 SocketIO.invoke 새로하므로 false로 해도 무방 => false로 해도 서버 재시작시 1초안에 여러건의 로깅이 발생함 (클라이언트는 옵션을 못찾음)
 	allowEIO3: false, autoConnect: false, pingTimeout: PING_TIMEOUT, pingInterval: PING_INTERVAL, cors: { origin: config.app.corsSocket, methods: ["GET", "POST"] }
 })
 io.adapter(redisAdapter(global.pub, sub))
