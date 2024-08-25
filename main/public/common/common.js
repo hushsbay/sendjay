241d9f1ca18d9e1b15a09b3c7a5006228afd8d91
hushsbay <hushsbay@gmail.com>
2024-08-25 14:33:31 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -1148,7 +1148,7 @@
             screenHeightOnLoad : null, //모바일 키보드 올라오면서 높이 조정
             chk : (param) => { //const param = new URLSearchParams(location.search)
                 const _os = param.get("webview")
-                alert(_os+"*****")
+                alert(_os+"*****"+type+"*****"+type+"*****"+msgid+"*****"+body)
                 if (_os == "and" || _os == "ios") {
                     hush.webview.on = true
                     if (_os == "ios") {
