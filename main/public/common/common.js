54ce28768a77dcb3589a3abcd3502b5dd02a70fd
hushsbay <hushsbay@gmail.com>
2024-09-08 09:30:32 +0900

    .

--- main/public/common/common.js
+++ main/public/common/common.js
@@ -275,23 +275,19 @@
                 return _ret
             },
             base64ToBlob(b64Data, contentType = 'image/png', sliceSize = 512) {
-                const byteCharacters = atob(b64Data);
-                const byteArrays = [];
-
+                const byteCharacters = atob(b64Data)
+                const byteArrays = []
                 for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-                    const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-                    const byteNumbers = new Array(slice.length);
+                    const slice = byteCharacters.slice(offset, offset + sliceSize)
+                    const byteNumbers = new Array(slice.length)
                     for (let i = 0; i < slice.length; i += 1) {
-                    byteNumbers[i] = slice.charCodeAt(i);
+                        byteNumbers[i] = slice.charCodeAt(i)
                     }
-
-                    const byteArray = new Uint8Array(byteNumbers);
-                    byteArrays.push(byteArray);
+                    const byteArray = new Uint8Array(byteNumbers)
+                    byteArrays.push(byteArray)
                 }
-
-                const blob = new Blob(byteArrays, { type: contentType });
-                return blob;
+                const blob = new Blob(byteArrays, { type: contentType })
+                return blob
              }
         },
         http : {
