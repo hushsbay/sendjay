cc3e37fb1195a1d836fe818497dba58ae3100a24
hushsbay <hushsbay@gmail.com>
2024-09-11 10:04:08 +0900

    .

--- main/route/user/userlist_test.js
+++ main/route/user/userlist_test.js
@@ -15,6 +15,7 @@ router.post('/', async function(req, res) {
 	try {
 		const rs = ws.http.resInit()
 		const { keyword, sort } = req.body
+		console.log(keyword, sort, "===========")
 		conn = await wsmysql.getConnFromPool(global.pool) //의도적으로 인증체크하지 않음
 		sql =  "SELECT ID_KIND, ORG_CD, ORG_NM, TOP_ORG_CD, TOP_ORG_NM, USER_ID, USER_NM, NICK_NM, JOB, TEL_NO, AB_CD, AB_NM "
 		sql += "  FROM Z_USER_TBL "
